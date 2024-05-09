347241dcf17b325d9160eeff5a1d343eb1aff32e
national-salvation-party <156296708+national-salvation-party@users.noreply.github.com>
2024-05-10 02:26:21 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/',methods=["GET","POST"])
 def hello_world():
     #res=requests.get("https://satanist.pythonanywhere.com/niitowainagure")
-    return render_template('templates/index.html',hit_num="0")
+    return "hello"#render_template('index.html',hit_num="0")
 
 @app.route("/hit",methods=["GET","POST"])
 def hit():
     #requests.post("https://satanist.pythonanywhere.com/niitowainagure")
-    return redirect("/")
+    return "hello"#redirect("/")
 #if __name__ == "__main__":
 #    app.run()
