0c5f8d6edcec9913d8728b02e716d334a05313db
national-salvation-party <156296708+national-salvation-party@users.noreply.github.com>
2024-05-10 02:06:53 +0900

    [fix] defined acceptable methods

---
+++ api/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/',methods=["GET","POST"])
+def hello_world():
+    res=requests.get("https://satanist.pythonanywhere.com/niitowainagure")
+    return render_template('index.html',hit_num=res.text)
+
+@app.route("/hit",methods=["GET","POST"])
+def hit():
+    requests.post("https://satanist.pythonanywhere.com/niitowainagure")
+    return redirect("/")
+if __name__ == "__main__":
+    app.run()
